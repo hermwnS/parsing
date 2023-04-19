f5c5d57aad411b854e2c3efa43a38518552d7a5a
hermwnS <126874286+hermwnS@users.noreply.github.com>
2023-04-19 14:35:09 +0700

    menambahkan parsing integer

--- parsing/parsing/Program.cs
+++ parsing/parsing/Program.cs
@@ -2,6 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Masukkan angka: ");
+        string inputString = Console.ReadLine();
+        try
+        {
+            int inputInt = Convert.ToInt32(inputString);
+            Console.WriteLine(che(inputInt * inputInt));
+        }
+        catch(FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch(OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
